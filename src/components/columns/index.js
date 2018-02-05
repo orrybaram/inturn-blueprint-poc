e99d8d2a3fb478f838573447c20816e4b7929aaa
orrybaram <orrybaram@gmail.com>
2018-02-05 17:14:26 -0500

    Updates

--- src/components/columns/index.js
+++
@@ -1,3 +0,0 @@
-export { default as NumberSortableColumn } from './NumberSortableColumn';
-export { default as TextSortableColumn } from './TextSortableColumn';
-export { default as SelectAllColumn } from './SelectAllColumn';
